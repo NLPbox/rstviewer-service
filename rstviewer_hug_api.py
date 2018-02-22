0d257a016edc7de524977765cda6db716ff5358b
Arne Neumann <nlpbox.programming@arne.cl>
2018-02-22 11:18:06 +0100

    now there are two endpoints: rs3_to_html, rs3_to_png

--- rstviewer_hug_api.py
+++ rstviewer_hug_api.py
@@ -17,8 +17,8 @@ def process_data(request, response, resource):
     response.set_header('Access-Control-Allow-Origin', '*')
 
 
-@hug.post('/convert', output=hug.output_format.file)
-def call_converter(body, output_format):
+@hug.post('/rs3_to_png', output=hug.output_format.file)
+def rs3_to_png(body):
     converter = sh.Command(CONVERTER_EXECUTABLE)
 
     if 'input' in body:
@@ -26,15 +26,24 @@ def call_converter(body, output_format):
         with open(INPUT_FILEPATH, 'wb') as input_file:
             input_file.write(input_file_content)
 
-        if output_format == b'rstviewer-html':
-            output_filepath = INPUT_FILEPATH+'.html'
-            converter_stdout = converter('-f', 'html', input_file.name, output_filepath)
-            return output_filepath
-        
-        else: # always fall back to default format rstviewer-png
-            output_filepath = INPUT_FILEPATH+'.png'
-            converter_stdout = converter('-f', 'png', input_file.name, output_filepath)
-            return output_filepath
-    
+        output_filepath = INPUT_FILEPATH+'.png'
+        converter_stdout = converter('-f', 'png', input_file.name, output_filepath)
+        return output_filepath
+    else:
+        return {'body': body}
+
+
+@hug.post('/rs3_to_html', output=hug.output_format.html)
+def rs3_to_html(body):
+    converter = sh.Command(CONVERTER_EXECUTABLE)
+
+    if 'input' in body:
+        input_file_content = body['input']
+        with open(INPUT_FILEPATH, 'wb') as input_file:
+            input_file.write(input_file_content)
+
+        output_filepath = INPUT_FILEPATH+'.html'
+        converter_stdout = converter('-f', 'html', input_file.name, output_filepath)
+        return output_filepath
     else:
         return {'body': body}
